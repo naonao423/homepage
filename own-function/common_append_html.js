e4bbbaccbacf2e6d7ddc6d2af479cc214a10d883
naonao423 <https://github.com/naonao423/naonao423.github.io.git>
2020-12-26 22:19:10 +0900

    donetableのanchor化

--- own-function/common_append_html.js
+++ own-function/common_append_html.js
@@ -3,6 +3,34 @@
 // navbar
 
 function append_html_content(){
+  var session_id = $.cookie("session_id")
+  console.log(session_id)
+  if (session_id == "test"){
+  append_content = `
+                <li class="nav-item active">
+                  <a class="nav-link" href="homepage.html">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="programing.html">Programming</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about_me.html">About me</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="donetable.html">Todos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="datas.html">Data</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="setting.html">Setting</a>
+                </li>
+                </li>
+                <li class="nav-item">
+                  <button type="button" class="text-white nav-link btn btn-outline-danger" id="logout">Logout</button>
+                </li>
+            `
+          }else{
   append_content = `
                 <li class="nav-item active">
                   <a class="nav-link" href="homepage.html">Home<span class="sr-only">(current)</span></a>
@@ -30,6 +58,7 @@ function append_html_content(){
                   <button type="button" class="text-white nav-link btn btn-outline-danger" id="logout">Logout</button>
                 </li>
             `
+  }
   $('#navcontent').append(append_content)
   $("#summary").text("tests")
 }
--- own-function/login.js
+++ own-function/login.js
@@ -33,6 +33,40 @@ function detect(){
   }
 }
 
+function detect_only_me(){
+  send_file = {}
+  var session_id = $.cookie("session_id")
+  console.log(session_id)
+  if ( session_id == undefined || session_id == "test" ){
+    location.href = './login.html'
+  } else if (session_id != "test"){
+    send_file["session_id"] = session_id
+    console.log(send_file)
+    $.ajax({
+      "url":check_url,
+      "type":"POST",
+      data: JSON.stringify(send_file),
+      contentType:'application/json'
+  }).done(function(data){
+    console.log(data)
+    if (data['error'] == "no_session_id"){
+      location.href = '/login.html'
+    }
+    $('#answer').text(data['user_id'])
+    ret = data['user_id']
+    guest(ret)
+    return ret
+    test()
+  }).fail(function(xhr, textStatus, errorThrown) {
+        console.log("NG:" + textStatus.status);
+        location.href = '/login.html'
+        ret = 0
+        return ret
+    }
+  )
+  }
+}
+
 
 function guest(ret){
   $("#web_title").text(ret)
